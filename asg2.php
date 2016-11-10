705271ba2fd3271e977c63211b01e06bb35b81dd
preeti-webonise <preeti.bhosale@weboniselab.com>
2016-11-10 16:36:10 +0530

    updates code for REST API assignment

--- asg2.php
+++ asg2.php
@@ -1,45 +1,121 @@
 
 <?php
-$category=array('a'=>clothes,'b'=>grocery,'c'=>bags);
-function display_category($category)
+$inventory=array('a'=>tshirt,'b'=>watch,'c'=>trousers,'d'=>vegetables,'e'=>milk);
+$cart=array('tshirt'=>200,'trousers'=>500,'milk'=>45);
+function display_inventory($inventory)
 {
- return json_encode($category);
+ return json_encode($inventory);
 }
-function delete_category($category)
+function display_cart($cart)
 {
-  unset($category);
- //echo "array is now empty";
+ echo json_encode($cart);
+ $total=array_sum($cart);
+ return ($total);
 }
-function insert_category($category)
+
+function delete_inventory($inventory)
+{
+  unset($inventory);
+}
+function delete_cart($cart)
+{
+  unset($cart);
+}
+function insert_inventory($inventory)
 {
  $postarray=$_POST;
- $newarray=array_merge($category,$postarray);
- return json_encode($newarray);
-
+ $newarray=array_merge($inventory,$postarray);
+ echo json_encode($newarray);
 }
-function update_category($category)
+function insert_cart($cart)
 {
- $putarray=$_REQUEST;
- $replacearray=array_replace($category,$putarray);
- return json_encode($replacearray);
+ $postarray=$_POST;
+ $newarray=array_merge($cart,$postarray);
+ echo json_encode($newarray);
+ $total=array_sum($newarray);
+ return ($total);
+}
+function update_inventory($inventory)
+{
+	$replace_item=$_REQUEST;
+	foreach($inventory as $key=>$value)
+	{
+		if($replace_item['name']==$key)
+		{
+			$newValue=$replace_item['value'];
+			$rep = array(''.$key => $newValue );
+			$put_inventory=array_replace_recursive($inventory, $rep);
+		}
+	}
+	return json_encode($put_inventory);
 
 }
+function update_cart($cart)
+{ 
+        $replace_item=$_REQUEST;
+	foreach($cart as $key=>$value)
+	{
+		if($replace_item['name']==$key)
+		{
+			$newValue=$replace_item['value'];
+			$rep = array(''.$key => $newValue );
+			$put_cart=array_replace_recursive($cart, $rep);
+		}
+	}
+        echo json_encode($put_cart);
+	$total=array_sum($put_cart);
+        return ($total);
+}
+
 if($_SERVER['REQUEST_METHOD']=='GET')
 {
-  echo display_category($category);
+  $id=$_GET['id'];
+  if($id=="inventory")
+  {
+   echo display_inventory($inventory);
+  }
+  if($id=="cart")
+  {
+   echo display_cart($cart);
+  }
 }
 if($_SERVER['REQUEST_METHOD']=='DELETE')
 {
- delete_category($category);
- echo "array is now empty";
+  $id=$_GET['id'];
+  if($id=="inventory")
+  {
+    delete_inventory($inventory);
+   echo "array is now empty";
+  }
+  if($id=="cart")
+  {
+    delete_cart($cart);
+   echo "array is now empty";
+  }
 }
 if($_SERVER['REQUEST_METHOD']=='POST')
 {
- echo insert_category($category);
+ $id=$_GET['id'];
+ if($id=="inventory")
+  {
+    echo insert_inventory($inventory);
+  }
+  if($id=="cart")
+  {
+    echo insert_cart($cart);
+  }
 }
 if($_SERVER['REQUEST_METHOD']=='PUT')
 {
- echo update_category($category);
+ $id=$_GET['id'];
+ if($id=="inventory")
+  {
+   echo update_inventory($inventory);
+  }
+  if($id=="cart")
+  {
+   echo update_cart($cart);
+  }
 }
 ?>
 
