85e8bb0c0cfa553da2b2941ccbc92c360bf3301d
preeti-webonise <preeti.bhosale@weboniselab.com>
2016-11-29 18:38:51 +0530

    Update AddressesTable.php

--- StudentsApplication/src/Model/Table/AddressesTable.php
+++ StudentsApplication/src/Model/Table/AddressesTable.php
@@ -5,24 +5,10 @@ use Cake\ORM\Table;
 
 class AddressesTable extends Table
 {
-
-    /*public function initialize(array $config)
-    {
-
-    $this->table('pincodes');
-	
-	 /*$this->hasMany('students', [
-            'className' => 'StudentsTable',
-            'foreignKey' => 'pincode',
-            ]);*/
-		//$this->belongsTo('Students');
-    //}*/
-
-    public function initialize(array $config)
+     public function initialize(array $config)
     {
         $this->table('addresses');
         $this->belongsTo('Students', [
-            //'foreignKey' => 'student_id',
             'dependent' => true,
         ]);
     }
