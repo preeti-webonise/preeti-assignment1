e36ac6c70533d1c5574cdfb6da305f1b748a71ea
preeti-webonise <preeti.bhosale@weboniselab.com>
2016-12-05 16:36:42 +0530

    updates hangman assignment

--- hangman/hangman.php
+++ hangman/hangman.php
@@ -1,168 +1,137 @@
 <?php
-require_once 'hangedman.php';
-$category="household things";
-$words=array();
-$numwords=0;
-$script=" ";
+	require_once 'hangedman.php';
+	$category = "household things";
+	$words = array();
+	$numWords = 0;
+    $script = " ";
 
-function get_words()
-{
-	global $words,$numwords;
-	$file_content=fopen("words.txt","r");
-	
-	while(true)
-	{
-		$word=fgets($file_content);
-		if(!$word) break;
-		$words[]=rtrim($word,"\n");
-		$numwords++;
+	function getWords() {
+		global $words,$numWords;
+		$fileContent = fopen("words.txt","r");
+		while(true) {
+			$word=fgets($fileContent);
+			if(!$word) break;
+			$words[] = rtrim($word,"\n");
+			$numWords++;
+		}
+		fclose($fileContent);
 	}
-	fclose($file_content);
-}
 
-function play_game()
-{
-	global $words;
-	global $numwords;
-	global $hanged;
-	global $category;
-	global $script;
-	global $guess_template;
-	$choose_word=rand(0,$numwords-1);
-	$choosed_word=$words[$choose_word];
-	$guess_template=str_repeat(" _ ",strlen($choosed_word));
-	$script= $_SERVER["PHP_SELF"];
-	
-	printpage($category,$hanged[0], $guess_template, $choose_word, "", 0);
-}
-
-function printpage($category,$image,$guess_template,$choose_word,$guessed,$wrong)
-{
-global $script;
-global $guess_template;
-	echo <<<END
-   	<!DOCTYPE html>
-	<html>
- 	<head>
-		<title>Hangman game</title>
- 	</head>
-	
-	<body>
- 	<h1>Hangman Game</h1>
- 	<br>
-	<p>category of word:$category</p>
-	<br>
- 	<pre>$image</pre>
-  	<br>
-	<p>Use A to Z to alphabets to guess</p>
- 	<p><strong>Word to guess: $guess_template</strong></p>
- 	<p>Letters used in guesses so far: $guessed</p>
-	<form method="post" action="$script">
-		<input type="hidden" name="wrong" value="$wrong" /><br>
-		<input type="hidden" name="lettersguessed" value="$guessed" /><br>
-		<input type="hidden" name="word" value="$choose_word" />
-		<label>Your next guess</label>
-	 	 <input type="text" name="letter"/>
-	 	 <input type="submit" value="Guess" />
-		
-	</form>
-	</body>
-	</html>
-END;
-}
-
-function check_letter($word,$letters_guessed)
-{
-	global $guess_template;
-	$len = strlen($word);
-	$guess_template = str_repeat("_",$len);
-	for ($i = 0; $i < $len; $i++)
-	{
-		$ch = $word[$i];
-		if (stristr($letters_guessed, $ch))
-		 {
-	 		 $pos = $i;
-	 		 $guess_template[$pos] = $ch;
-		 }
+	function playGame() {
+		global $words;
+		global $numWords;
+		global $hanged;
+		global $category;
+		global $script;
+		global $guessTemplate;
+		$chooseWord = rand(0,$numWords-1);
+		$choosedWord = $words[$chooseWord];
+		$guessTemplate = str_repeat(" _ ",strlen($choosedWord));
+		$script = $_SERVER["PHP_SELF"];
+		printPage($category,$hanged[0], $guessTemplate, $chooseWord, "", 0);
 	}
-	return $guess_template;
-}
 
-function handleguess() 
-{
-	global $words;
-	global $hanged;
-	global $category;
-	global $guess_tamplate;
-  	$choose_word = $_POST["word"];
-  	$word  = $words[$choose_word];
-  	$wrong = $_POST["wrong"];
-  	$letters_guessed = $_POST["lettersguessed"];
-  	$guess = $_POST["letter"];
-  	$letter =$guess[0];
-  	if(!stristr($word, $letter)) 
-	{
-		$wrong++;
-  	}	
-  	$letters_guessed = $letters_guessed . $letter;
-  	$guess_template = check_letter($word, $letters_guessed);
-  	if (!stristr($guess_template, "_")) 
-	{
-		game_won($word);
-  	} 
-	else if ($wrong >= 5) 
-	{
-		game_lost($word);
-  	}
-	 else 
-	{
-		printpage($category,$hanged[$wrong], $guess_template, $choose_word, $letters_guessed,$wrong);
-  	}
-}
-
-function game_won($word)
-{
-	echo <<<END
-	<!DOCTYPE html>
-	<html>
-  	<head>
-	<title>Hangman game</title>
-  	</head>
-  	<body>
-	<h1>You win!</h1>
-	<pYou guessed that the word was $word</p>
- 	 </body>
-	</html>
+	function printPage($category,$image,$guessTemplate,$chooseWord,$guessed,$wrong) { 
+		global $script;
+		global $guessTemplate;
+		echo <<<END
+   		<!DOCTYPE html>
+		<html>
+ 		<head><title>Hangman game</title></head>
+		<body>
+ 			<h1>Hangman Game</h1>
+ 			<br>
+			<p>category of word:$category</p>
+			<br>
+ 			<pre>$image</pre>
+  			<br>
+			<p>Use A to Z to alphabets to guess</p>
+ 			<p><strong>Word to guess: $guessTemplate</strong></p>
+ 			<p>Letters used in guesses so far: $guessed</p>
+			<form method = "post" action = "$script">
+				<input type = "hidden" name = "wrong" 		   value = "$wrong" /><br>
+				<input type = "hidden" name = "lettersguessed" value = "$guessed" /><br>
+				<input type = "hidden" name = "word"   		   value = "$chooseWord" />
+				<label>Your next guess</label>
+	 	 		<input type = "text" name = "letter"/>
+	 	 		<input type = "submit" value = "Guess" />
+			</form>
+		</body>
+	    </html>
 END;
-}
+	}
 
-function game_lost($word)
-{
-	echo <<<END
-	<!DOCTYPE html>
+	function checkLetter($word,$lettersGuessed) {
+		global $guessTemplate;
+		$len = strlen($word);
+		$guessTemplate = str_repeat("_",$len);
+		for ($i = 0; $i < $len; $i++) {
+			$ch = $word[$i];
+			if (stristr($lettersGuessed, $ch)) {
+		 		 $pos = $i;
+		 		 $guessTemplate[$pos] = $ch;
+			 	}
+			}
+		return $guessTemplate;
+	}
+
+	function handleGuess() {
+		global $words;
+		global $hanged;
+		global $category;
+		global $guessTemplate;
+	  	$chooseWord = $_POST["word"];
+	  	$word  = $words[$chooseWord];
+	  	$wrong = $_POST["wrong"];
+	  	$lettersGuessed = $_POST["lettersguessed"];
+	  	$guess = $_POST["letter"];
+	  	$letter =$guess[0];
+	  	if(!stristr($word, $letter)) {
+			$wrong++;
+	  	}	
+	  	$lettersGuessed = $lettersGuessed . $letter;
+	  	$guessTemplate = checkLetter($word, $lettersGuessed);
+	  	if (!stristr($guessTemplate, "_")) {
+			gameWon($word);
+	  	} else if ($wrong >= 5) {
+			gameLost($word);
+	  	} else {
+			printPage($category,$hanged[$wrong], $guessTemplate, $chooseWord, $lettersGuessed,$wrong);
+	  	}
+	}
+
+	function gameWon($word) {
+		echo <<<END
+		<!DOCTYPE html>
+		<html>
+  		<head><title>Hangman game</title></head>
+  		<body>
+			<h1>You win!</h1>
+			<pYou guessed that the word was $word</p>
+ 	 	</body>
+		</html>
+END;
+	}
+
+	function gameLost($word) {
+		echo <<<END
+		<!DOCTYPE html>
         <html>
-        <head>
-        <title>Hangman game</title>
-        </head>
+        <head><title>Hangman game</title></head>
         <body>
-        <h1>You lose!</h1>
-        <p>word you were trying to guess was $word</p>
-         </body>
+        	<h1>You lose!</h1>
+       	 	<p>word you were trying to guess was $word</p>
+        </body>
         </html>
 END;
+	}
 
-}
+	getWords();
 
-get_words();
-
-$method=$_SERVER["REQUEST_METHOD"];
-if($method=="POST")
-{
-	handleguess();
-}
-else
-{
-	play_game();
-}
-
+	$method = $_SERVER["REQUEST_METHOD"];
+	if($method == "POST") {
+		handleGuess();
+	}else {
+		playGame();
+	}
 ?>	
